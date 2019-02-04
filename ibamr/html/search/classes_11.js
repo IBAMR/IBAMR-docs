e58e7f3d608253bf0d6a0f23c6f7cc0a82e5ed68
Travis CI User <travis@example.org>
2019-02-04 03:49:30 +0000

    Deploy code docs to GitHub Pages Travis build: 150
    Commit: adf8631882cd6d6ba8dbe939538110d23d246957

--- ibamr/html/search/classes_11.js
+++ ibamr/html/search/classes_11.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['quadratic_5fprobe_5ffn',['quadratic_probe_fn',['http://gcc.gnu.org/onlinedocs/libstdc++/latest-doxygen/a10068.html',1,'__gnu_pbds']]],
+  ['quadraturecache',['QuadratureCache',['../class_i_b_t_k_1_1_quadrature_cache.html',1,'IBTK']]],
   ['queue',['queue',['http://gcc.gnu.org/onlinedocs/libstdc++/latest-doxygen/a06760.html',1,'std']]],
   ['quicksort_5ftag',['quicksort_tag',['http://gcc.gnu.org/onlinedocs/libstdc++/latest-doxygen/a08680.html',1,'__gnu_parallel']]]
 ];
--- ibamr/html/search/variables_11.js
+++ ibamr/html/search/variables_11.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['qsb_5fsteals',['qsb_steals',['http://gcc.gnu.org/onlinedocs/libstdc++/latest-doxygen/a08624.html#ad2087c371858b65ed5af5070e82fc80b',1,'__gnu_parallel::_Settings']]],
-  ['quadratures',['quadratures',['../class_i_b_t_k_1_1_f_e_cache.html#a940dac23af94ddbbd5bf818931175caf',1,'IBTK::FECache::quadratures()'],['../class_i_b_t_k_1_1_f_e_map_cache.html#af52150f768a9d351e81727aa0ddce6c6',1,'IBTK::FEMapCache::quadratures()']]]
+  ['quadrature_5fcache',['quadrature_cache',['../class_i_b_t_k_1_1_f_e_cache.html#a3bf09bb497304d248ee4bb130b608bfd',1,'IBTK::FECache::quadrature_cache()'],['../class_i_b_t_k_1_1_f_e_map_cache.html#a68462ce2f9618e0acaf5ae83b6354a74',1,'IBTK::FEMapCache::quadrature_cache()']]],
+  ['quadratures',['quadratures',['../class_i_b_t_k_1_1_quadrature_cache.html#a684f476ab817c4f5d34c4d5578a52f38',1,'IBTK::QuadratureCache']]]
 ];
